ab1dfc4acfea760dfaef83857592d6e1e39e55bf
Johannes Kling <johannes.kling92@gmail.com>
2016-09-03 22:55:43 +0200

    added router, initial layout, added login/signup form

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,10 +1,24 @@
 import {NgModule} from "@angular/core";
 import {BrowserModule} from "@angular/platform-browser";
 import {AppComponent} from "./app.component";
+import {FormsModule} from "@angular/forms";
+import {LoginComponent} from "./login/login.component";
+import {RouterModule, Routes} from "@angular/router";
+import {LandingComponent} from "./landing/landing.component";
+import {SignupComponent} from "./signup/signup.component";
+
+
+const routes = [
+  {path: '', component: LandingComponent,children:[
+    {path: '', component: LoginComponent},
+    {path: 'signup', component: SignupComponent}
+  ]}
+];
 
 @NgModule({
-    imports: [ BrowserModule ],
-    declarations: [ AppComponent ],
-    bootstrap:    [ AppComponent ]
-})
-export class AppModule { }
+  imports: [BrowserModule, FormsModule, RouterModule,RouterModule.forRoot(routes)],
+  declarations: [AppComponent, LoginComponent, LandingComponent, SignupComponent],
+  bootstrap: [AppComponent]
+  })
+export class AppModule {
+}
