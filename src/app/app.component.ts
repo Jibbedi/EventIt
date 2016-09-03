52241469e8c4d3dbe324d5c9c0432c9f07703f98
Johannes Kling <johannes.kling92@gmail.com>
2016-09-03 23:04:56 +0200

    fixed background fading issue

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -8,47 +8,4 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
 
-  prefix = "url('./images/";
-  suffix = "')";
-
-  firstBackgroundVisible = true;
-
-  firstBackgroundImageUrl;
-  secondBackgroundImageUrl;
-
-  backgroundImageNames = ['bg0.jpg','bg1.jpg','bg2.jpg','bg3.jpg'];
-  backgroundIndex = 1;
-
-  constructor() {
-
-    this.firstBackgroundImageUrl = this.buildBackgroundUrl(0);
-    this.secondBackgroundImageUrl = this.buildBackgroundUrl(1);
-
-    window.setInterval(() => {
-      this.firstBackgroundVisible = !this.firstBackgroundVisible;
-      this.switchBackgroundImage();
-
-    },20000);
-  }
-
-  private switchBackgroundImage() {
-    window.setTimeout(() => {
-      this.backgroundIndex = (this.backgroundIndex + 1) % this.backgroundImageNames.length;
-
-      if (this.firstBackgroundVisible) {
-        this.secondBackgroundImageUrl = this.buildBackgroundUrl(this.backgroundIndex);
-      }
-      else {
-        this.firstBackgroundImageUrl = this.buildBackgroundUrl(this.backgroundIndex);
-      }
-
-      console.log(this.secondBackgroundImageUrl,this.firstBackgroundImageUrl);
-
-    }, 4000);
-  }
-
-  private buildBackgroundUrl(index: number) {
-    return this.prefix + this.backgroundImageNames[index] + this.suffix;
-  }
-
 }
--- src/app/landing/landing.component.ts
+++ src/app/landing/landing.component.ts
@@ -6,11 +6,49 @@ import { Component, OnInit } from '@angular/core';
   templateUrl: 'landing.component.html',
   styleUrls: ['landing.component.css']
 })
-export class LandingComponent implements OnInit {
+export class LandingComponent {
 
-  constructor() { }
+  prefix = "url('./images/";
+  suffix = "')";
 
-  ngOnInit() {
+  firstBackgroundVisible = true;
+
+  firstBackgroundImageUrl;
+  secondBackgroundImageUrl;
+
+  backgroundImageNames = ['bg0.jpg','bg1.jpg','bg2.jpg','bg3.jpg'];
+  backgroundIndex = 1;
+
+  constructor() {
+
+    this.firstBackgroundImageUrl = this.buildBackgroundUrl(0);
+    this.secondBackgroundImageUrl = this.buildBackgroundUrl(1);
+
+    window.setInterval(() => {
+      this.firstBackgroundVisible = !this.firstBackgroundVisible;
+      this.switchBackgroundImage();
+
+    },20000);
+  }
+
+  private switchBackgroundImage() {
+    window.setTimeout(() => {
+      this.backgroundIndex = (this.backgroundIndex + 1) % this.backgroundImageNames.length;
+
+      if (this.firstBackgroundVisible) {
+        this.secondBackgroundImageUrl = this.buildBackgroundUrl(this.backgroundIndex);
+      }
+      else {
+        this.firstBackgroundImageUrl = this.buildBackgroundUrl(this.backgroundIndex);
+      }
+
+      console.log(this.secondBackgroundImageUrl,this.firstBackgroundImageUrl);
+
+    }, 4000);
+  }
+
+  private buildBackgroundUrl(index: number) {
+    return this.prefix + this.backgroundImageNames[index] + this.suffix;
   }
 
 }
