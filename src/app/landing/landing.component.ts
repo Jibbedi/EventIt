9091a54cf3ee76fb661df0f48565667b605e6cf6
Johannes Kling <johannes.kling92@gmail.com>
2016-09-03 22:56:55 +0200

    added landing page, added signup and login components

---
+++ src/app/landing/landing.component.ts
@@ -0,0 +1,16 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  moduleId: module.id,
+  selector: 'landing-component',
+  templateUrl: 'landing.component.html',
+  styleUrls: ['landing.component.css']
+})
+export class LandingComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
