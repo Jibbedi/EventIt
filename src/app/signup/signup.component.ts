f8efd2eb9fa90d8d9fd37b095a42fcc46f9de342
Johannes Kling <johannes.kling92@gmail.com>
2016-09-04 18:04:43 +0200

    added show password functionality

--- src/app/signup/signup.component.ts
+++ src/app/signup/signup.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit } from '@angular/core';
+import {Component, OnInit} from '@angular/core';
 import {UserService} from "../shared/user-service/user.service";
 import {FormBuilder, FormGroup, Validators} from "@angular/forms";
 
@@ -8,7 +8,7 @@ import {FormBuilder, FormGroup, Validators} from "@angular/forms";
   templateUrl: 'signup.component.html',
   styleUrls: ['signup.component.css']
 })
-export class SignupComponent  {
+export class SignupComponent {
 
   signUpForm: FormGroup;
   signUpError;
@@ -16,18 +16,35 @@ export class SignupComponent  {
   constructor(fb: FormBuilder, private _userService: UserService) {
     this.signUpForm = fb.group({
       email: [this._userService.prefilledData.email, Validators.compose([Validators.pattern("^[^@]+?\@[^@\.]+?\.[A-Za-z]+?$"), Validators.required])],
-      password: [this._userService.prefilledData.password, Validators.required]
+      password: [this._userService.prefilledData.password, Validators.required],
+      visiblePassword: [this._userService.prefilledData.password, Validators.required],
+      showPassword: [false]
     });
+
+
+    this.syncValueOfControls('password', 'visiblePassword');
+
+
   }
+
 
   signUp() {
     if (!this.signUpForm.valid) return;
 
     this._userService.signUp(this.getValueOfControlWithName('email'), this.getValueOfControlWithName('password'))
-      .then(loginSuccessful => console.log(loginSuccessful),failure => this.signUpError = failure);
+      .then(loginSuccessful => console.log(loginSuccessful), failure => this.signUpError = failure);
   }
 
   private getValueOfControlWithName(name) {
     return this.signUpForm.controls[name].value
+  }
+
+  private syncValueOfControls(first, second) {
+    this.updateValueOfControlOnChangeOfOtherControl(first, second);
+    this.updateValueOfControlOnChangeOfOtherControl(second, first);
+  }
+
+  private updateValueOfControlOnChangeOfOtherControl(first, second) {
+    this.signUpForm.controls[first].valueChanges.subscribe(v => this.signUpForm.controls[second].setValue(this.signUpForm.controls[first].value,{emitEvent : false}));
   }
 }
